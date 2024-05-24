d341c28bb801fedebae99b937937af5a285f625d
wangjiayu <1303183851@qq.com>
2024-05-25 00:19:52 +0800

    微服务

---
+++ docker_practice/hmall/cart-service/src/main/java/com/hmall/cart/HMallApplication.java
@@ -0,0 +1,13 @@
+package com.hmall.cart;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@MapperScan("com.hmall.mapper")
+@SpringBootApplication
+public class HMallApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HMallApplication.class, args);
+    }
+}
